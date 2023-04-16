fd6aa1b944a40a9778a5f40a822a62f2720a9b5f
tuancaurao <tuan.pt@orai.io>
2023-04-16 18:39:07 +0700

    Test AggregateRoot

---
+++ test/domain/AggregateRoot_test.py
@@ -0,0 +1,47 @@
+from uuid import uuid4
+
+from returns.result import Result, Success
+
+from dino_seedwork_be.domain.AggregateRoot import AggregateRoot
+from dino_seedwork_be.domain.Entity import BaseRawAttributes
+from dino_seedwork_be.domain.value_object.UUID import UUID
+
+
+class ExampleAggAttributes(BaseRawAttributes):
+    attribute_a: int
+    attribute_b: str
+
+
+class ExampleAggUUID(AggregateRoot[ExampleAggAttributes, UUID]):
+    _attribute_a: int
+    _attribute_b: str
+
+    def attribute_a(self) -> int:
+        return self._attribute_a
+
+    def attribute_b(self) -> str:
+        return self._attribute_b
+
+    def from_atributes(self, raw_attributes: ExampleAggAttributes) -> Result:
+        self._attribute_a = raw_attributes["attribute_a"]
+        self._attribute_b = raw_attributes["attribute_b"]
+        return Success(None)
+
+
+class TestAggregate:
+    def test_created_at_auto(self):
+        new_id = UUID(uuid4())
+        example_agg = ExampleAggUUID.create(
+            {
+                "attribute_a": 1,
+                "attribute_b": "tuanpham",
+                "created_at": None,
+                "updated_at": None,
+            },
+            new_id,
+        ).unwrap()
+
+        assert example_agg.created_at() is not None
+        assert example_agg.attribute_a() == 1
+        assert example_agg.attribute_b() == "tuanpham"
+        assert example_agg.identity() == new_id
